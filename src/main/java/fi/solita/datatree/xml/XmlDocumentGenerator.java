c50a7d8fc6b2276e732e0a1456c14b40a8529aaa
Esko Luontola <esko.luontola@solita.fi>
2013-09-07 23:26:35 +0300

    Refactored: extracted XmlDocumentGenerator, exposed reusable sub-steps

---
+++ src/main/java/fi/solita/datatree/xml/XmlDocumentGenerator.java
@@ -0,0 +1,48 @@
+// Copyright © 2013 Solita Oy <www.solita.fi>
+// This software is released under the MIT License.
+// The license text is at http://opensource.org/licenses/MIT
+
+package fi.solita.datatree.xml;
+
+import fi.solita.datatree.*;
+import org.w3c.dom.*;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+
+public class XmlDocumentGenerator {
+
+    public static void toXml(Tree tree, Result result) throws TransformerException {
+        Document document = toDocument(tree);
+        TransformerFactory.newInstance().newTransformer().transform(new DOMSource(document), result);
+    }
+
+    public static Document toDocument(Tree tree) {
+        try {
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+            document.setXmlStandalone(true);
+            appendElement(document, document, tree);
+            return document;
+
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Makes {@code tree} a child node of {@code parent}.
+     */
+    public static void appendElement(Document document, Node parent, Tree tree) {
+        Element current = document.createElement(tree.name());
+        current.appendChild(document.createTextNode(tree.content()));
+        parent.appendChild(current);
+
+        for (Meta meta : tree.metae()) {
+            current.setAttribute(meta.name(), meta.value());
+        }
+        for (Tree child : tree.children()) {
+            appendElement(document, current, child);
+        }
+    }
+}
--- src/test/java/fi/solita/datatree/xml/XmlDocumentGeneratorTest.java
+++ src/test/java/fi/solita/datatree/xml/XmlDocumentGeneratorTest.java
@@ -2,14 +2,12 @@
 // This software is released under the MIT License.
 // The license text is at http://opensource.org/licenses/MIT
 
-package fi.solita.datatree;
+package fi.solita.datatree.xml;
 
+import fi.solita.datatree.Tree;
 import org.junit.Test;
-import org.w3c.dom.*;
 
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamResult;
 import java.io.StringWriter;
 
@@ -17,7 +15,7 @@ import static fi.solita.datatree.Tree.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class GeneratingXmlDocumentsTest {
+public class XmlDocumentGeneratorTest {
 
     private static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 
@@ -55,30 +53,12 @@ public class GeneratingXmlDocumentsTest {
 
     private static String toXml(Tree tree) {
         try {
-            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-            document.setXmlStandalone(true);
-
-            append(document, document, tree);
-
             StringWriter result = new StringWriter();
-            TransformerFactory.newInstance().newTransformer().transform(new DOMSource(document), new StreamResult(result));
+            XmlDocumentGenerator.toXml(tree, new StreamResult(result));
             return result.toString();
 
-        } catch (ParserConfigurationException | TransformerException e) {
+        } catch (TransformerException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private static void append(Document doc, Node parent, Tree tree) {
-        Element current = doc.createElement(tree.name());
-        current.appendChild(doc.createTextNode(tree.content()));
-        parent.appendChild(current);
-
-        for (Meta meta : tree.metae()) {
-            current.setAttribute(meta.name(), meta.value());
-        }
-        for (Tree child : tree.children()) {
-            append(doc, current, child);
         }
     }
 }
