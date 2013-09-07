c08962a3604d32c7186d87d9110f54b45ffc4d89
Esko Luontola <esko.luontola@solita.fi>
2013-09-07 23:17:00 +0300

    Generating XML, the rest

--- src/test/java/fi/solita/datatree/GeneratingXmlDocumentsTest.java
+++ src/test/java/fi/solita/datatree/GeneratingXmlDocumentsTest.java
@@ -13,16 +13,44 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.StringWriter;
 
-import static fi.solita.datatree.Tree.tree;
+import static fi.solita.datatree.Tree.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class GeneratingXmlDocumentsTest {
 
+    private static final String HEADER = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+
     @Test
-    public void simple_document() {
-        assertThat(toXml(tree("foo", "bar")),
-                is("<?xml version=\"1.0\" encoding=\"UTF-8\"?><foo>bar</foo>"));
+    public void empty_elements() {
+        assertThat(toXml(
+                tree("foo")),
+                is(HEADER + "<foo/>"));
+    }
+
+    @Test
+    public void text_content() {
+        assertThat(toXml(
+                tree("foo", "bar")),
+                is(HEADER + "<foo>bar</foo>"));
+    }
+
+    @Test
+    public void nested_elements() {
+        assertThat(toXml(
+                tree("root",
+                        tree("foo", "1"),
+                        tree("bar", "2"))),
+                is(HEADER + "<root><foo>1</foo><bar>2</bar></root>"));
+    }
+
+    @Test
+    public void attributes() {
+        assertThat(toXml(
+                tree("root",
+                        meta("a", "1"),
+                        meta("b", "2"))),
+                is(HEADER + "<root a=\"1\" b=\"2\"/>"));
     }
 
     private static String toXml(Tree tree) {
@@ -30,9 +58,7 @@ public class GeneratingXmlDocumentsTest {
             Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
             document.setXmlStandalone(true);
 
-            Element root = document.createElement(tree.name());
-            root.appendChild(document.createTextNode(tree.content()));
-            document.appendChild(root);
+            append(document, document, tree);
 
             StringWriter result = new StringWriter();
             TransformerFactory.newInstance().newTransformer().transform(new DOMSource(document), new StreamResult(result));
@@ -40,6 +66,19 @@ public class GeneratingXmlDocumentsTest {
 
         } catch (ParserConfigurationException | TransformerException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void append(Document doc, Node parent, Tree tree) {
+        Element current = doc.createElement(tree.name());
+        current.appendChild(doc.createTextNode(tree.content()));
+        parent.appendChild(current);
+
+        for (Meta meta : tree.metae()) {
+            current.setAttribute(meta.name(), meta.value());
+        }
+        for (Tree child : tree.children()) {
+            append(doc, current, child);
         }
     }
 }
