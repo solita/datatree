15f745e8659f55c8662cbc9c72a7513b359a8d48
Esko Luontola <esko.luontola@solita.fi>
2013-09-10 16:57:24 +0300

    Fixed releasing the OSSRH staging repository

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,9 +4,14 @@ set -eu
 DESCRIPTION="$1"
 set -x
 
+# For some reason nexus-staging:release does not read the stagingRepositoryId
+# from the properties file, so we must read it ourselves.
+REPOSITORY_ID=`sed -n -r 's/stagingRepository\.id=(\w+)/\1/p' "staging/ff5044adfb72.properties"`
+
 mvn nexus-staging:release \
     --errors \
     -DaltStagingDirectory=staging \
+    -DstagingRepositoryId="$REPOSITORY_ID" \
     -DstagingDescription="$DESCRIPTION"
 
 git push origin HEAD
