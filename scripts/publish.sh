ffe198a9b8cd82357c83229bd2887428b9e94c07
Esko Luontola <esko.luontola@solita.fi>
2013-09-10 15:39:08 +0300

    Release OSSRH and push to GitHub automatically

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,15 +4,10 @@ set -eu
 DESCRIPTION="$1"
 set -x
 
-# TODO: release OSSRH and push to GitHub automatically
-#mvn nexus-staging:release \
-#    --errors \
-#    -DaltStagingDirectory=staging \
-#    -DstagingDescription="$DESCRIPTION"
+mvn nexus-staging:release \
+    --errors \
+    -DaltStagingDirectory=staging \
+    -DstagingDescription="$DESCRIPTION"
 
-set +x
-echo ""
-echo "Done. Next steps:"
-echo "    open https://oss.sonatype.org/"
-echo "    git push origin HEAD"
-echo "    git push origin --tags"
+git push origin HEAD
+git push origin --tags
